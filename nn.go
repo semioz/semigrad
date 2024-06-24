2be92b2a5cc1e113444b2ccc196aff966397f736
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2024-06-24 22:32:24 +0300

    some additions

--- nn.go
+++ nn.go
@@ -38,6 +38,20 @@ func (n *Neuron) NeuronParams() []*Value {
 	return params
 }
 
+func (n *Neuron) Forward(inputs []*Value) *Value {
+	// Sum of the product of the weights and the inputs
+	act := n.bias
+	//weighted sum of inputs plus the bias
+	for i, w := range n.weights {
+		act = act.Add(w.Mul(inputs[i]))
+	}
+	if n.nonLin {
+		//introduces non-linearity into the network, allowing it to learn more complex patterns
+		return act.Relu()
+	}
+	return act
+}
+
 func NewLayer(nin, nout int) *Layer {
 	neurons := make([]*Neuron, nout)
 	for i := 0; i < nout; i++ {
@@ -69,4 +83,27 @@ func (m *MLP) MLPParams() []*Value {
 		params = append(params, l.LayerParams()...)
 	}
 	return params
+}
+
+// TODO
+func (m *MLP) Loss() {}
+
+// gotta .zero_grad() before backward
+func (m *MLP) ZeroGrad() {
+	for _, p := range m.MLPParams() {
+		p.grad = 0
+	}
+}
+
+// Forward pass -> Reset Grad -> Backward pass -> Update(Gradient Descent)
+// Minimizing the loss through gradient descent
+func (m *MLP) Optimize() {
+	// Forward pass: input data is passed through the network. Each layer computes its output using its current weights and biases.
+	// Compute loss: The output of the network is compared to the target output using a loss function. A common loss function for regression problems is the mean squared error.
+
+	// Reset Grad
+
+	// Backward pass
+
+	// Update
 }
